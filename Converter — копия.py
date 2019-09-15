1fd5778991e1250ba563fcb42c4246292dcfe1f4
AlanTheKnight <51376126+AlanTheKnight@users.noreply.github.com>
2019-09-15 10:57:00 +0300

    Закомментирован код

--- Converter — копия.py
+++ Converter — копия.py
@@ -1,18 +1,14 @@
 letters = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-def checkstring(text):
-	for i in text:
-		if not i in letters:
-			return False
-	return True
-
-
 def convert_to_decimal(a,abase):
+	# a - строка, число которое переводим
+	# abase - число, база a
 	return int(a, abase)
 
 def convert_to_y(ybase,xdecimal):
 	# ybase - база числа, которое хотим получить на выходе
 	# xdecimal - число, которое переводим - в десятичной системе счисления
+	# ОБА АРГУМЕНТА - ЦЕЛЫЕ ЧИСЛА
 	output = []
 	num = xdecimal
 	while True:
@@ -23,9 +19,11 @@ def convert_to_y(ybase,xdecimal):
 			output.append(num % ybase)
 			num = num // ybase
 	output.reverse()
+	# checklist() проверяет, есть ли в списке output числа больше 9 и 
+	# заменяет их буквенной записью, а также переводит все элементы списка
+	# в строку
 	checklist(output)
-	# Переводим все элементы списка в строки
-	# Сливаем элементы списка в строку, сохраняем в output
+	# Переводим все элементы списка в строку
 	output = "".join(output)
 	return(output)
 
@@ -37,5 +35,8 @@ def checklist(catalog):
 		catalog[count] = str(sym)
 	return catalog
 
-number = "z"
-print(convert_to_y(16, 250))
+number = 1000
+print(convert_to_y(2, number))
+
+number = "FA"
+print(convert_to_decimal(number, 16))
